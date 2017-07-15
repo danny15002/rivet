b7f4c328ea0f4355346a4267524502b3a920b498
Daniel Bellini <daniel@USGadmins-MacBook-Pro.local>
2017-07-14 20:09:04 -0500

    slight responsive

--- app/Components/PicMenu.jsx
+++ app/Components/PicMenu.jsx
@@ -3,8 +3,14 @@ class PicMenu extends React.Component {
     super(props);
   }
 
+  toggleHeart() {
+
+  }
+
   render() {
     return <div className="pic-menu">
+      <span onClick={this.toggleHeart} className="fa fa-heart fa-2x hover-pointer"/>
+      <span onClick={this.toggleHeart} className="fa fa-share-alt-square fa-2x hover-pointer"/>
     </div>;
   }
 }
